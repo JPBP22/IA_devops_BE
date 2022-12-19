bd5cd121b8c08688d405be6ad484135f293c3095
JPBP22 <79642745+JPBP22@users.noreply.github.com>
2022-12-19 16:56:00 +0100

    Update test_model.py

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -7,9 +7,9 @@ def test_create_recipe():
     WHEN a new Recipe is created
     THEN check the name, ingredients, steps, rating and favorite fields are defined correctly
     """
-    recipe = Recipe('Pop tart', 'Large Eggs, Salt, Butter', 'C, serve', 5, True)
+    recipe = Recipe('Pop tart', 'Large Eggs, Salt, Butter, Cinammon, Flour', 'Crack eggs, pour eggs, mix with flower, put into oven, bake ten minutes, bring out, put cinamon, cool down', 3, True)
     assert recipe.name == 'Pop tart'
     assert recipe.ingredients == 'Large Eggs, Salt, Butter, Cinammon, Flour'
-    assert recipe.steps == 'Crack eggs, pour eggs,mix with flower, put into oven at 250, bake 10 minutes, bring out, put cinamon, cool down'
+    assert recipe.steps == 'Crack eggs, pour eggs, mix with flower, put into oven, bake ten minutes, bring out, put cinamon, cool down'
     assert recipe.rating == 3
     assert recipe.favorite == True
